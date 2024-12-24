69389e6b55315c87481defdd6e9901d9ca6eeae5
nwabosibeko <nwabosibeko17@gmail.com>
2024-12-24 16:03:38 +0200

    NumberGuess complete.

--- NumberGuess/src/main/java/RunGame.java
+++ NumberGuess/src/main/java/RunGame.java
@@ -1,5 +1,15 @@
-public class RunGame {
-    
+import java.util.Scanner;
 
+public class RunGame {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String playerName = scanner.nextLine();
+        Game game = new Game(playerName);
+        game.anotherRound();
+
+    }
 
 }
