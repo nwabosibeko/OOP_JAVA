c63910b2510b661d2eb0a81b0a9d030a8ec8fd74
nwabosibeko <nwabosibeko17@gmail.com>
2024-12-26 17:58:45 +0200

    Implementing Learner class.

--- Grader/src/main/java/Learner.java
+++ Grader/src/main/java/Learner.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Learner {
+
+    private String name;
+    private ArrayList<Subject> subjects;
+
+    public Learner(String studentName) {
+        name = studentName;
+        subjects = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addSubject(String subjectName, double mark) {
+        subjects.add(new Subject(subjectName, mark));
+    }
+
+    public ArrayList<Subject> getSubjects() {
+        return subjects;
+    }
+
 }
