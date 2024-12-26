76f7d174309bdca1d4cb393be46800e3768cf5a9
nwabosibeko <nwabosibeko17@gmail.com>
2024-12-26 17:49:34 +0200

    Constructor added.

--- Grader/src/main/java/Subject.java
+++ Grader/src/main/java/Subject.java
@@ -1,2 +1,17 @@
 public class Subject {
+
+    private String subName;
+    private double mark;
+
+    public Subject(String subject, double subMark){
+
+        subName = subject;
+        mark = subMark;
+
+
+
+    }
+
+
+
 }
